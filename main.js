d1c42b4190cd5ebadfd0d2b97c3a27457c46778d
Helencarrillo <esmeraldacarrillo.2029@gmail.com>
2025-04-23 14:42:31 -0600

    main.js

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 // Selecciona todos los botones con la clase 'btn'
 constante botones = documento.consultaSelectorAll('
 
-botones 
+botones.para cada uno(boton =>{
+  boton.añadirListener de eventos('hacer clic',()
+    boton.lista de clases.agregar('hizo clic');
+  
+  establecer tiempo de espera (()=> {
+    boton.lista de clases.agregar('hizo clic');
+  }, 300);
+});
+});
+
